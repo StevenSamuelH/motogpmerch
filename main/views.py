35894714eff607d34192549b5c23a9a428a8ec95
Raihan Fitra Setyabudi <raihan.fitra18@gmail.com>
2022-04-22 15:44:26 +0700

    edit views dan template

--- main/views.py
+++ main/views.py
@@ -1,5 +1,15 @@
+from urllib import response
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from daftar_penjual.models import Merchant
+from profil_produk.models import Product
+from review.models import ReviewProduk
 
 def home(request):
     return render(request, 'main/home.html')
+
+def produk(request):
+    product = Product.objects.all()
+    review = ReviewProduk.objects.all()
+    response = {'products' : product}
+    return render(request, 'daftar_produk.html', response)
