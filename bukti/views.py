50d51f077642ca52e4095e912481f95c48b207a1
Bintang Hari Pratama <79510565+bintangbhp@users.noreply.github.com>
2022-04-22 21:45:50 +0700

    Update views.py

--- bukti/views.py
+++ bukti/views.py
@@ -9,6 +9,6 @@ def add_data(request):
     if request.method == "POST":
         if data_pembeli.is_valid():
             data_pembeli.save()
-            return http.HttpResponseRedirect('https://motogpmerch.herokuapp.com/')
+            return http.HttpResponseRedirect('')
     response = {'form' : data_pembeli}
     return render(request, 'bukti_pembayaran.html', response)
