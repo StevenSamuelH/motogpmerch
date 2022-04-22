569d81e59a154d7a62fa1b6b6d67bde50984df1e
bintangbhp <bhp.bintang@gmail.com>
2022-04-22 18:33:16 +0700

    mengganti halaman bukti pembayaran

--- bukti/forms.py
+++ bukti/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import DataPembeli
+from django.utils.translation import ugettext_lazy as _
 
 class DataPembeliForm(forms.ModelForm):
     class Meta:
         model = DataPembeli
         fields = "__all__"
+        # Ganti label
+        labels = {
+            'nama' : _('Nama Pembeli'),
+            'alamat' : _('Alamat Pembeli'),
+            'bukti_img' : _('Upload Bukti Pembayaran'),
+        }
