c617bb6eacd93f25dea6eb5b6ae19d3df802af71
daffa.aldin <daffa.aldin@gmail.com>
2022-06-10 22:46:53 +0700

    Review add json

--- review/views.py
+++ review/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.core import serializers
 from .models import ReviewProduk
 
 def index(request):
     produk = ReviewProduk.objects.all()
     response = {'index': produk}
     return render(request, 'review_produk.html', response)
+
+def json(request):
+    produk = ReviewProduk.objects.all()
+    data = serializers.serialize('json', produk)
+    return HttpResponse(data, content_type="application/json")
