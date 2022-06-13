698f8ec6e188e5a33725be122485addd85b2c056
daffa.aldin <daffa.aldin@gmail.com>
2022-06-13 10:41:49 +0700

    Change in Review Models

--- review/models.py
+++ review/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class ReviewProduk(models.Model):
     nama = models.CharField(max_length=100)
-    gambar = models.ImageField(upload_to='motogpmerch-images')
+    gambar = models.URLField()
     review = models.TextField()
-    rating = models.ImageField(upload_to='motogpmerch-images')
+    rating = models.URLField()
